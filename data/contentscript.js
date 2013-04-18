489ead75c4a19798fc332c71dce5a0168ddde623
Andrei Hajdukewycz <sancus@off.net>
2013-04-18 15:51:15 -0400

    add mapping array for Bugzilla Products to Kanban Project IDs

--- data/contentscript.js
+++ data/contentscript.js
@@ -1,44 +1,60 @@
 
 var kbAPIKeyName = 'kbAPIKey';
 var kbWorkspace = 'mozilla';
-var kbProjectId = '33256';
 var addon_self = self;
 
+var mapping = [{"bzProduct": "www.mozilla.org",
+                "bzComponent": "",
+                "kanProjectId": 33256}]
+
 function getProduct() {
-    var bzProduct = $('#product option:selected').val();
+    var product = $('#product option:selected').val();
     // maybe not logged in
-    if ( ! bzProduct ) {
-        bzProduct = $('#field_container_product').text();
+    if ( ! product ) {
+        product = $('#field_container_product').text();
     }
-    return bzProduct;
+    return product;
 }
 
-if ( getProduct() === "www.mozilla.org" ) {
-    var $button = $('<a id="kanbutton" href="#"><span></span></a>');
-    $button.insertAfter('#summary_alias_container');
+function getComponent() {
+    var component = $('#component option:selected').val();
+    // maybe not logged in
+    if ( ! component ) {
+        component = $('#field_container_component').text();
+    }
+    return component;
+}
 
-    $button.on('click', function(e){
-        e.preventDefault();
-        var apiKey = localStorage.getItem(kbAPIKeyName);
-        if ( ! apiKey ) {
-            apiKey = prompt('Kanbanery API Key Please');
-            localStorage.setItem(kbAPIKeyName, apiKey);
+function kanButton(e, kbProjectId) {
+    e.preventDefault();
+    var apiKey = localStorage.getItem(kbAPIKeyName);
+    if ( ! apiKey ) {
+        apiKey = prompt('Kanbanery API Key Please');
+        localStorage.setItem(kbAPIKeyName, apiKey);
+    }
+    if (apiKey) {
+        var reqURL = 'https://' + kbWorkspace +
+                     '.kanbanery.com/api/v1/projects/' +
+                     kbProjectId + '/icebox/tasks.json';
+        var cardData = {
+            'task[task_type_name]': 'Story',
+            'task[title]': $('#short_desc_nonedit_display').text(),
+            'task[description]': document.location.href
+        };
+        var reqData = {
+            url: reqURL,
+            headers: {'X-Kanbanery-ApiToken': apiKey},
+            content: cardData
+        };
+        addon_self.port.emit('callKB', reqData);
+    }  
+}
+for (let i of mapping) {
+    if ( getProduct() === i.bzProduct ) {
+        if (i.bzComponent === "") {
+            var $button = $('<a id="kanbutton" href="#"><span></span></a>');
+            $button.insertAfter('#summary_alias_container');
+            $button.on('click', function (e) { kanButton(e, i.kanProjectId); })
         }
-        if (apiKey) {
-            var reqURL = 'https://' + kbWorkspace +
-                         '.kanbanery.com/api/v1/projects/' +
-                         kbProjectId + '/icebox/tasks.json';
-            var cardData = {
-                'task[task_type_name]': 'Story',
-                'task[title]': $('#short_desc_nonedit_display').text(),
-                'task[description]': document.location.href
-            };
-            var reqData = {
-                url: reqURL,
-                headers: {'X-Kanbanery-ApiToken': apiKey},
-                content: cardData
-            };
-            addon_self.port.emit('callKB', reqData);
-        }
-    })
+    }
 }
